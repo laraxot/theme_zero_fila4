65acf7f56462c7dea7cadc1fe6c6e3f4f0e0bcca
marco76tv <marco.sottana@gmail.com>
2025-09-21 00:14:09 +0200

    .

--- laravel/app/Application.php
+++ laravel/app/Application.php
@@ -1,25 +1,21 @@
 <?php
-
+/**
+* ---.
+*/
 declare(strict_types=1);
 
 namespace App;
-
-use Safe\Exceptions\FilesystemException;
-use function Safe\realpath;
 
 class Application extends \Illuminate\Foundation\Application
 {
     public function publicPath($path = ''): string
     {
-        $tmp = $this->basePath . '/../public_html/' . $path;
+        $tmp = $this->basePath.'/../public_html/'.$path;
         $tmp = str_replace(['/', '\\'], [DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR], $tmp);
-        
-        try {
-            return realpath($tmp);
-        } catch (FilesystemException $e) {
-            // Fallback to base path
-            $basePublicPath = realpath($this->basePath . '/../public_html/');
-            return $basePublicPath . '/' . $path;
+        if (realpath($tmp) === false) {
+            return realpath($this->basePath.'/../public_html/').'/'.$path;
         }
+
+        return realpath($tmp);
     }
 }
