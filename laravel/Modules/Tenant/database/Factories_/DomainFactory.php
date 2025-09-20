d5431ccbd840ec507c951f12f74dbc78dfa00125
marco76tv <marco.sottana@gmail.com>
2025-09-20 23:59:11 +0200

    .

---
+++ laravel/Modules/Tenant/database/Factories_/DomainFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Tenant\Database\Factories;
+
+use Modules\Tenant\Models\Domain;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DomainFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     */
+    protected $model = Domain::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [];
+    }
+}
