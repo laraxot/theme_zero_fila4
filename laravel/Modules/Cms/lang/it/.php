9c784e2ce569ed7292ce4d66f2910a6ff4940e8f
marco76tv <marco.sottana@gmail.com>
2025-09-20 23:59:11 +0200

    .

---
+++ laravel/Modules/Cms/lang/it/.php
@@ -0,0 +1,270 @@
+<?php
+
+return array (
+  'fields' => 
+  array (
+    'title' => 
+    array (
+      'label' => 'title',
+    ),
+    'slug' => 
+    array (
+      'label' => 'slug',
+    ),
+    'items' => 
+    array (
+      'label' => 'items',
+    ),
+    'label' => 
+    array (
+      'label' => 'label',
+    ),
+    'url' => 
+    array (
+      'label' => 'url',
+    ),
+    'style' => 
+    array (
+      'label' => 'style',
+    ),
+    'icon' => 
+    array (
+      'label' => 'icon',
+    ),
+    'size' => 
+    array (
+      'label' => 'size',
+    ),
+    'alignment' => 
+    array (
+      'label' => 'alignment',
+    ),
+    'gap' => 
+    array (
+      'label' => 'gap',
+    ),
+    'view' => 
+    array (
+      'label' => 'view',
+    ),
+    'description' => 
+    array (
+      'label' => 'description',
+    ),
+    'email' => 
+    array (
+      'label' => 'email',
+    ),
+    'phone' => 
+    array (
+      'label' => 'phone',
+    ),
+    'address' => 
+    array (
+      'label' => 'address',
+    ),
+    'map_url' => 
+    array (
+      'label' => 'map_url',
+    ),
+    'button_text' => 
+    array (
+      'label' => 'button_text',
+    ),
+    'button_link' => 
+    array (
+      'label' => 'button_link',
+    ),
+    'sections' => 
+    array (
+      'label' => 'sections',
+    ),
+    'subtitle' => 
+    array (
+      'label' => 'subtitle',
+    ),
+    'image' => 
+    array (
+      'label' => 'image',
+    ),
+    'cta_text' => 
+    array (
+      'label' => 'cta_text',
+    ),
+    'cta_link' => 
+    array (
+      'label' => 'cta_link',
+    ),
+    'background_color' => 
+    array (
+      'label' => 'background_color',
+    ),
+    'text_color' => 
+    array (
+      'label' => 'text_color',
+    ),
+    'cta_color' => 
+    array (
+      'label' => 'cta_color',
+    ),
+    'logo' => 
+    array (
+      'label' => 'logo',
+    ),
+    'copyright' => 
+    array (
+      'label' => 'copyright',
+    ),
+    'links' => 
+    array (
+      'label' => 'links',
+    ),
+    'alt' => 
+    array (
+      'label' => 'alt',
+    ),
+    'text' => 
+    array (
+      'label' => 'text',
+    ),
+    'type' => 
+    array (
+      'label' => 'type',
+    ),
+    'width' => 
+    array (
+      'label' => 'width',
+    ),
+    'height' => 
+    array (
+      'label' => 'height',
+    ),
+    'children' => 
+    array (
+      'label' => 'children',
+    ),
+    'orientation' => 
+    array (
+      'label' => 'orientation',
+    ),
+    'placeholder' => 
+    array (
+      'label' => 'placeholder',
+    ),
+    'success_message' => 
+    array (
+      'label' => 'success_message',
+    ),
+    'error_message' => 
+    array (
+      'label' => 'error_message',
+    ),
+    'content' => 
+    array (
+      'label' => 'content',
+    ),
+    'target' => 
+    array (
+      'label' => 'target',
+    ),
+    'social_links' => 
+    array (
+      'label' => 'social_links',
+    ),
+    'platform' => 
+    array (
+      'label' => 'platform',
+    ),
+    'stats' => 
+    array (
+      'label' => 'stats',
+    ),
+    'number' => 
+    array (
+      'label' => 'number',
+    ),
+    'level' => 
+    array (
+      'label' => 'level',
+    ),
+    'background' => 
+    array (
+      'label' => 'background',
+    ),
+    'buttons' => 
+    array (
+      'label' => 'buttons',
+    ),
+    'class' => 
+    array (
+      'label' => 'class',
+    ),
+    'link' => 
+    array (
+      'label' => 'link',
+    ),
+    'ratio' => 
+    array (
+      'label' => 'ratio',
+    ),
+    'caption' => 
+    array (
+      'label' => 'caption',
+    ),
+    'img_uuid' => 
+    array (
+      'label' => 'img_uuid',
+    ),
+    'gallery' => 
+    array (
+      'label' => 'gallery',
+    ),
+    'version' => 
+    array (
+      'label' => 'version',
+    ),
+    'method' => 
+    array (
+      'label' => 'method',
+    ),
+    'video' => 
+    array (
+      'label' => 'video',
+    ),
+    'content_blocks' => 
+    array (
+      'label' => 'content_blocks',
+    ),
+    'sidebar_blocks' => 
+    array (
+      'label' => 'sidebar_blocks',
+    ),
+    'footer_blocks' => 
+    array (
+      'label' => 'footer_blocks',
+    ),
+  ),
+  'actions' => 
+  array (
+    'activeLocale' => 
+    array (
+      'label' => 'activeLocale',
+    ),
+    'save' => 
+    array (
+      'label' => 'save',
+    ),
+    'cancel' => 
+    array (
+      'label' => 'cancel',
+    ),
+  ),
+  'sections' => 
+  array (
+    'empty' => 
+    array (
+      'heading' => '',
+      'label' => 'empty',
+    ),
+  ),
+);
