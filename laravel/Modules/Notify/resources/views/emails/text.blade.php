fccf62ecffa52c60229a60e0bac5d7a3d48750bb
marco76tv <marco.sottana@gmail.com>
2025-09-23 21:32:02 +0200

    .

--- laravel/Modules/Notify/resources/views/emails/text.blade.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-?>
-{{ $email_data->body }}
--- laravel/Modules/UI/lang/it/components.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'state_icons' => [
-        'no_transitions' => 'Nessuna transizione disponibile',
-    ],
-];
