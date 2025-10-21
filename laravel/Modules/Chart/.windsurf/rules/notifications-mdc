d78c35a92ca45f15a033268f558757a498f00f07
marco76tv <marco.sottana@gmail.com>
2025-10-21 18:44:23 +0200

    .

--- laravel/Modules/Chart/.windsurf/rules/notifications-mdc
+++
@@ -1,5 +0,0 @@
----
-trigger: manual
-description:
-globs:
----
--- laravel/Modules/Chart/bashscripts/lib/logging.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Funzione di logging
-log() {
-    local message="$1"
-    local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
-    echo "[$timestamp] $message"
-} 
