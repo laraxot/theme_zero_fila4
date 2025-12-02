c639a8b2c5d938741c9e300ffcf647652caf97ff
marco76tv <marco.sottana@gmail.com>
2025-12-02 16:24:34 +0100

    .

--- laravel/Modules/Notify/resources/views/filament/clusters/test/pages/slack-notification.blade.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-?>
-<x-filament-panels::page>
-
-</x-filament-panels::page>
--- laravel/Modules/Quaeris/resources/views/filament/widgets/filters.blade.php
+++
@@ -1,6 +0,0 @@
-<x-filament-widgets::widget>
-    <x-filament::section>
-        {{-- Widget content --}}
-        {{ $this->form }}
-    </x-filament::section>
-</x-filament-widgets::widget>
